e5fa7664749a89147ac00206268028992df42ee3
John Carlo Pierce <johncarlopierce@gmail.com>
2023-08-30 20:49:50 -0400

    create UserSearchForm component; partials for font and colors; import font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import UserList from './components/UserList/UserList';
 import UserForm from "./components/UserForm/UserForm";
+import UserSearchForm from './components/UserSearchForm/UserSearchForm';
 
 function App() {
   return (
     <>
       <UserList />
       <UserForm />
+      <UserSearchForm />
     </>
   );
 }
